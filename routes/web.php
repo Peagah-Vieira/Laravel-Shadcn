ae8df6af92a88f95a11961d0ed0899f4e724075b
Peagah <peagahvieira2003@gmail.com>
2024-09-02 21:50:16 -0300

    :sparkles: Shadcn/ui->Example

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,4 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('test', [TestController::class, 'test']);
+
+require __DIR__ . '/auth.php';
