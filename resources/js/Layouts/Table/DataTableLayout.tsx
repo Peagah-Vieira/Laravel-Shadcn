29e284fe38c0fab5f5662494382b2fab2cb02af4
Peagah <peagahvieira2003@gmail.com>
2024-09-06 17:36:43 -0300

    :sparkles: Dashboard/Users->DataTable->Layout->Create User|Dialog

--- resources/js/Layouts/Table/DataTableLayout.tsx
+++ resources/js/Layouts/Table/DataTableLayout.tsx
@@ -17,9 +17,21 @@ import {
     TableHeader,
     TableRow,
 } from "@/Components/ui/table"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter
+} from "@/Components/ui/dialog"
 import { Input } from "@/Components/ui/input"
+import { Label } from "@/Components/ui/label"
 import { DataTablePagination } from "./DataTablePaginationLayout"
 import { DataTableViewOptions } from "./DataTableOptionsLayout"
+import { Button } from "@/Components/ui/button"
+import { PlusCircle } from "lucide-react"
 import * as React from "react"
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -60,7 +72,7 @@ export function DataTable<TData, TValue>({
 
     return (
         <div>
-            <div className="flex items-center py-4">
+            <div className="ml-auto flex items-center gap-2 py-4">
                 <Input
                     placeholder="Filter..."
                     value={globalFilter ?? ''}
@@ -69,6 +81,49 @@ export function DataTable<TData, TValue>({
                     }
                     className="max-w-sm"
                 />
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button size="sm" className="h-8 gap-1">
+                            <PlusCircle className="h-3.5 w-3.5" />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                Create User
+                            </span>
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Edit profile</DialogTitle>
+                            <DialogDescription>
+                                Make changes to your profile here. Click save when you're done.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="name" className="text-right">
+                                    Name
+                                </Label>
+                                <Input
+                                    id="name"
+                                    defaultValue="Pedro Duarte"
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="username" className="text-right">
+                                    Username
+                                </Label>
+                                <Input
+                                    id="username"
+                                    defaultValue="@peduarte"
+                                    className="col-span-3"
+                                />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <Button type="submit">Save changes</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
                 <DataTableViewOptions table={table} />
             </div>
             <div className="rounded-md border">
