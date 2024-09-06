0d866f9526bb9515095e5f43edfe811a6cfc02e5
Peagah <peagahvieira2003@gmail.com>
2024-09-05 23:40:51 -0300

    :sparkles: Dashboard/Users->DataTable->Row Selection Create

--- resources/js/Layouts/DataTable.tsx
+++ resources/js/Layouts/DataTable.tsx
@@ -45,6 +45,8 @@ export function DataTable<TData, TValue>({
     const [columnVisibility, setColumnVisibility] =
         React.useState<VisibilityState>({})
 
+    const [rowSelection, setRowSelection] = React.useState({})
+
     const table = useReactTable({
         data,
         columns,
@@ -55,10 +57,12 @@ export function DataTable<TData, TValue>({
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
         onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
         state: {
             sorting,
             columnFilters,
             columnVisibility,
+            rowSelection,
         },
     })
 
@@ -145,7 +149,12 @@ export function DataTable<TData, TValue>({
                         )}
                     </TableBody>
                 </Table>
+
                 <div className="flex items-center justify-end space-x-2 p-4">
+                    <div className="flex-1 text-sm text-muted-foreground">
+                        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                        {table.getFilteredRowModel().rows.length} row(s) selected.
+                    </div>
                     <Button
                         variant="outline"
                         size="sm"
