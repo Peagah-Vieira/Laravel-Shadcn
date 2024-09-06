3aec78d346c982d4070ab7c1250a39e65edf8274
Peagah <peagahvieira2003@gmail.com>
2024-09-05 23:35:18 -0300

    :sparkles: Dashboard/Users->DataTable->Visibility Create

--- resources/js/Layouts/DataTable.tsx
+++ resources/js/Layouts/DataTable.tsx
@@ -3,6 +3,7 @@ import {
     ColumnFiltersState,
     flexRender,
     SortingState,
+    VisibilityState,
     getCoreRowModel,
     getFilteredRowModel,
     getPaginationRowModel,
@@ -18,6 +19,12 @@ import {
     TableHeader,
     TableRow,
 } from "@/Components/ui/table"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu"
 import { Button } from "@/Components/ui/button"
 import { Input } from "@/Components/ui/input"
 import * as React from "react"
@@ -35,6 +42,8 @@ export function DataTable<TData, TValue>({
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
         []
     )
+    const [columnVisibility, setColumnVisibility] =
+        React.useState<VisibilityState>({})
 
     const table = useReactTable({
         data,
@@ -45,25 +54,55 @@ export function DataTable<TData, TValue>({
         getSortedRowModel: getSortedRowModel(),
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
         state: {
             sorting,
             columnFilters,
+            columnVisibility,
         },
     })
 
     return (
         <div>
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Filter emails..."
+                    value={table.getColumn("email")?.getFilterValue() as string}
+                    onChange={(event) =>
+                        table.getColumn("email")?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="ml-auto">
+                            Columns
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        {table
+                            .getAllColumns()
+                            .filter(
+                                (column) => column.getCanHide()
+                            )
+                            .map((column) => {
+                                return (
+                                    <DropdownMenuCheckboxItem
+                                        key={column.id}
+                                        className="capitalize"
+                                        checked={column.getIsVisible()}
+                                        onCheckedChange={(value) =>
+                                            column.toggleVisibility(!!value)
+                                        }
+                                    >
+                                        {column.id}
+                                    </DropdownMenuCheckboxItem>
+                                )
+                            })}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
             <div className="rounded-md border">
-                <div className="flex items-center p-4">
-                    <Input
-                        placeholder="Filter emails..."
-                        value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-                        onChange={(event) =>
-                            table.getColumn("email")?.setFilterValue(event.target.value)
-                        }
-                        className="max-w-sm"
-                    />
-                </div>
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
